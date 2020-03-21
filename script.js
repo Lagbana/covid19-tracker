7b4ff4ee7bbeafff152d755fd99f0660eb21ec99
Dustin Irving <dustin.irving@gmail.com>
2020-03-21 14:19:59 -0400

    Created an event listener and function to display graph data

--- script.js
+++ script.js
@@ -1,60 +1,101 @@
-let ctx = document.getElementById('myChart').getContext('2d');
-let chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'COVID-19',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-let queryURL = 'https://corona.lmao.ninja/countries?sort=%5Bproperty%5D'
-
-function callAPI() {
-    fetch(queryURL)
-        .then(function(response) {
-            console.log(response)
-            return response.json()
-        })
-        .then(function(coronaCases) {
-            console.log(coronaCases)
-            let china = coronaCases[1].country
-            let chinaCases = coronaCases[1].cases
-            let chinaTodayCases = coronaCases[1].todayCases
-            let chinaDeaths = coronaCases[1].deaths
-            let chinaTodayDeaths = coronaCases[1].todayDeaths
-            let chinaRecovered = coronaCases[1].recovered
-            let chinaCritical = coronaCases[1].critical
-
-            let chinaCasesElement = document.getElementById("china-cases")
-            let chinaTodayCasesElement = document.getElementById("china-today-cases")
-            let chinaDeathsElement = document.getElementById("china-deaths")
-            let chinaTodayDeathsElement = document.getElementById("china-today-deaths")
-            let chinaRecoveredElement = document.getElementById("china-recoverd")
-            let chinaCriticalElement = document.getElementById("china-critical")
-
-            chinaCasesElement.textContent = "Cases: " + chinaCases
-            chinaTodayCasesElement.textContent = "Cases Today: " + chinaTodayCases
-            chinaDeathsElement.textContent = "Deaths: " + chinaDeaths
-            chinaTodayDeathsElement.textContent = "Deaths Today: " + chinaTodayDeaths
-            chinaRecoveredElement.textContent = "Recovered: " + chinaRecovered
-            chinaCriticalElement.textContent = "Critical: " + chinaCritical
-        })
+// Covid data object
+let covidData = {
+    countries: [],
+    cases: [],
+    todayCases: [],
+    deaths: [],
+    todayDeaths: [],
+    recovered: [],
+    active: [],
+    critical: [],
+    casesPerOneMillion: []
 }
 
-let chinaButton = document.getElementById("china")
-let chinaData = document.getElementById("china-data")
+// covid-19 API URL
+let queryURL = 'https://corona.lmao.ninja/countries?sort=%5Bproperty%5D'
 
-chinaButton.addEventListener("click", function() {
-    callAPI()
+// Re-Usable function to get any API data
+const getData = async(url) => {
+    const result = await fetch(url).then(response => response.json())
+    return result
+}
+
+// Function to get all our necessary data
+const populate = (coronaCases) => {
+    for (let index of coronaCases) {
+        covidData.countries.push(index.country) //-------------------> List of Countries
+        covidData.cases.push(index.cases) //-------------------> List of Cases
+        covidData.todayCases.push(index.todayCases) //-------------------> List of Today's Cases
+        covidData.deaths.push(index.deaths) //-------------------> List of Total Deaths
+        covidData.todayDeaths.push(index.todayDeaths) //-------------------> List of Today's Deaths
+        covidData.active.push(index.active) //----------------------> List of Active (need more context for "Active")
+        covidData.recovered.push(index.recovered) //-------------------> List of Recovered
+        covidData.critical.push(index.critical) //-------------------> List of Critical
+        covidData.casesPerOneMillion.push(index.casesPerOneMillion) //------------------> List of cases per one million (used for comparison
+    }
+    return covidData
+}
+
+// Call ALL functions inside this on Page Load
+document.addEventListener('DOMContentLoaded', async() => {
+
+    const data = await getData(queryURL)
+    covidData = populate(data)
 })
+
+// Put the graph details into a function ----> Dustin's Task
+
+
+let countriesSelector = document.getElementById('countries')
+
+let countryIndex;
+
+
+// Add an event listener based on what country the user clicks on
+// The CreateBarGraph function is called that correspponds to the country index the user chose
+countriesSelector.addEventListener("click", function() {
+    let countryName = event.target.getAttribute("value")
+    let countries = covidData.countries
+    for (let i = 0; i < countries.length; i++) {
+        let country = countries[i]
+        if (country === countryName) {
+            countryIndex = i
+        }
+    }
+    createBarGraph(countryIndex)
+})
+
+// From the object covid19, the different properties are retrieved
+// The bar graph is updated with that countries' data
+function createBarGraph(countryIndex) {
+    console.log(covidData)
+    let casesChart = covidData.cases[countryIndex]
+    let casesTodayChart = covidData.todayCases[countryIndex]
+    let deathsChart = covidData.deaths[countryIndex]
+    let deathsTodayChart = covidData.todayDeaths[countryIndex]
+    let recoveredChart = covidData.recovered[countryIndex]
+    let activeChart = covidData.active[countryIndex]
+    let criticalChart = covidData.critical[countryIndex]
+    let casesPerOneMillionChart = covidData.casesPerOneMillion[countryIndex]
+
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ['Cases', 'Cases Today', 'Deaths', 'Deaths Today', 'Recovered', 'Active', 'Critical', 'Cases per million'],
+            datasets: [{
+                label: 'COVID-19',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [casesChart, casesTodayChart, deathsChart, deathsTodayChart, recoveredChart, activeChart, criticalChart, casesPerOneMillionChart]
+
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
