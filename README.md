cd307dcf873f344ecdf99a1d73a202c810f64024
Larry_A <larryagbana@gmail.com>
2020-03-21 11:13:37 -0400

    pushed covid-19 api data into object

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # project-Team4
+
+#### About
+An application that provides real-time information on COVID-19 related data and tweets.
+
+
+#### Project View
+https://lagbana.github.io/project-Team4/
+
+#### Team Members
+- Dustin: https://github.com/dustinirving 
+- Larry: https://github.com/Lagbana
+- Kacey: https://github.com/K4c3y
---
+++ script.js
@@ -0,0 +1,75 @@
+
+// Put the graph details into a function ----> Dustin's Task
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'COVID-19',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: []
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+// Covid data object
+let covidData = {
+    countries:[],
+    cases:[],
+    todayCases:[],
+    deaths:[],
+    todayDeaths:[],
+    recovered:[],
+    active:[],
+    critical:[],
+    casesPerOneMillion:[]
+}
+
+// covid-19 API URL
+let queryURL = 'https://corona.lmao.ninja/countries?sort=%5Bproperty%5D'
+
+// Re-Usable function to get any API data
+const getData = async (url) => {
+    const result = await fetch(url).then(response => response.json())
+    return result
+}
+
+// Function to get all our necessary data
+const populate = (coronaCases) => {
+    for (let index of coronaCases){
+        covidData.countries.push(index.country) //-------------------> List of Countries
+        covidData.cases.push(index.cases) //-------------------> List of Cases
+        covidData.todayCases.push(index.todayCases) //-------------------> List of Today's Cases
+        covidData.deaths.push(index.deaths) //-------------------> List of Total Deaths
+        covidData.todayDeaths.push(index.todayDeaths) //-------------------> List of Today's Deaths
+        covidData.active.push(index.active) //----------------------> List of Active (need more context for "Active")
+        covidData.recovered.push(index.recovered) //-------------------> List of Recovered
+        covidData.critical.push(index.critical) //-------------------> List of Critical
+        covidData.casesPerOneMillion.push(index.casesPerOneMillion) //------------------> List of cases per one million (used for comparison
+    }
+    return covidData
+}
+
+// Call ALL functions inside this on Page Load
+document.addEventListener('DOMContentLoaded', async () => {
+
+    const data = await getData(queryURL)
+    covidData = populate(data)
+    console.log(covidData)
+
+
+})
+
+
+
+let chinaButton = document.getElementById("china")
+let chinaData = document.getElementById("china-data")
